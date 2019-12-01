e28498f6a5c177b5aced2fdf539a5c7d51ddb900
Noam <nflear@gmail.com>
2019-12-01 13:42:45 -0500

    Updates

---
+++ precache-manifest.6756c9919d667d07bedfc692ee620028.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a17113d257d53e4db1f6d545cfa697",
+    "url": "/DecentReads/index.html"
+  },
+  {
+    "revision": "9a1bb8a3442f59b5d4b2",
+    "url": "/DecentReads/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "89b7b8c1060c2c3acb83",
+    "url": "/DecentReads/static/css/main.62caa077.chunk.css"
+  },
+  {
+    "revision": "9a1bb8a3442f59b5d4b2",
+    "url": "/DecentReads/static/js/2.e4ae5269.chunk.js"
+  },
+  {
+    "revision": "89b7b8c1060c2c3acb83",
+    "url": "/DecentReads/static/js/main.29445c86.chunk.js"
+  },
+  {
+    "revision": "66f11bcff955a1804f5a",
+    "url": "/DecentReads/static/js/runtime-main.25c12423.js"
+  },
+  {
+    "revision": "885168884d2c2b492a635b533ae6d338",
+    "url": "/DecentReads/static/media/flow.88516888.jpg"
+  },
+  {
+    "revision": "8249c3243435b45920ec0394b1c1aaee",
+    "url": "/DecentReads/static/media/freeWill.8249c324.jpg"
+  },
+  {
+    "revision": "c1334cf0893e67d0a7532bb2635edc04",
+    "url": "/DecentReads/static/media/header.c1334cf0.jpg"
+  },
+  {
+    "revision": "9123ecd6fb92ce66544ab82d0e7e00f9",
+    "url": "/DecentReads/static/media/normPeople.9123ecd6.jpg"
+  },
+  {
+    "revision": "51dd05f82aa75fcf333770224cb65d50",
+    "url": "/DecentReads/static/media/oneWorld.51dd05f8.jpg"
+  },
+  {
+    "revision": "25c1093d4cd7dca800e64eb7ff850206",
+    "url": "/DecentReads/static/media/range.25c1093d.jpg"
+  }
+]);
